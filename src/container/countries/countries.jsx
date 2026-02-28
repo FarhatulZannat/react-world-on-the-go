d594fe3ca116a7d56e7399f51b583e5fbdb8a692
FarhatulZannat <ebinfaria@gmail.com>
2026-03-01 01:03:35 +0600

    conditionally class create

--- src/container/countries/countries.jsx
+++ src/container/countries/countries.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { use } from 'react'
+import Country from '../country/country'
+import './countries.css'
 
-const Countries = () => {       
+const Countries = ({countriesPromise}) => {    
+  const countriesData = use(countriesPromise)   
+  const countries = countriesData.countries
+  // console.log(countries)
+
   return (
-    <div>
-      <h1>In the countries..........</h1>
+    <div >
+      <h1>In the countries : {countries.length}</h1>
+      <div className='countries'>
+        {
+        countries.map(country => <Country key={country.cca3.cca3} country = {country}></Country>)
+      }
+      </div>
     </div>
   )
 }
---
+++ src/container/country/country.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './country.css'
+import { useState } from 'react'
+
+const Country = ({country}) => {
+
+  const [visited, setVisited] = useState(false)
+
+
+  const handleVisited = () =>{
+    // ----1st basic system
+    // if(visited){
+    //   setVisited(false)
+    // }
+    // else{
+    //   setVisited(true)
+    // }
+
+    // ----2nd system
+    // setVisited(visited ? false : true)
+
+    // -----3rd toggle---true hole false kore diccchi false hole true kore dicchi
+    setVisited(!visited)
+  }
+
+
+  return (
+    <div className={`country  ${visited ? 'country-visited' : 'country-not-visited' }`}>
+    {/* <div className={`country  ${visited && 'country-visited'}`}> */}
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>Name : {country.name.common}</h3>
+      <p>Population : {country.population.population}</p>
+
+      <p>Area : {country.area.area} {country.area.area >300000 ? '--Big Country' : '--Small Country'}</p>
+      
+      <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
+    </div>
+  )
+}
+
+export default Country
